289c00f3e0a112a1371c477250447cd55b8b3300
kalyan.Katta <kalyankatta85@gmail.com>
2018-06-03 00:54:28 -0500

    first class

---
+++ src/com/learn/basic/BasicJava.java
@@ -0,0 +1,11 @@
+package com.learn.basic;
+
+public class BasicJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("after long time");
+	}
+
+}
